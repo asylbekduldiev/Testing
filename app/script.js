1b68ba48c06df62feeaca3547407f9dfa243215b
Asyl <duldievasylbek@gmail.com>
2025-01-07 08:50:31 +0500

    Db,Json,REs

--- app/script.js
+++ app/script.js
@@ -1,5 +1,3 @@
-const { func } = require("prop-types");
-
 window.addEventListener('DOMContentLoaded', function() {
 
     // Tabs
@@ -130,11 +128,11 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 700000);
+    const modalTimerId = setTimeout(openModal, 300000);
     // Изменил значение, чтобы не отвлекало
 
     function showModalByScroll() {
-        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
             openModal();
             window.removeEventListener('scroll', showModalByScroll);
         }
@@ -184,32 +182,35 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        ".menu .container"
-    ).render();
+    getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
+        });
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        14,
-        ".menu .container"
-    ).render();
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => createCard(data));
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        21,
-        ".menu .container"
-    ).render();
+    // function createCard(data) {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         const element = document.createElement('div');
+
+    //         element.classList.add("menu__item");
+
+    //         element.innerHTML = `
+    //             <img src=${img} alt=${altimg}>
+    //             <h3 class="menu__item-subtitle">${title}</h3>
+    //             <div class="menu__item-descr">${descr}</div>
+    //             <div class="menu__item-divider"></div>
+    //             <div class="menu__item-price">
+    //                 <div class="menu__item-cost">Цена:</div>
+    //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //             </div>
+    //         `;
+    //         document.querySelector(".menu .container").append(element);
+    //     });
+    // }
 
     // Forms
 
@@ -221,10 +222,32 @@ window.addEventListener('DOMContentLoaded', function() {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        let res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+        });
+    
+        return await res.json();
+    };
+
+    async function getResource(url) {
+        let res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    }
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -236,47 +259,20 @@ window.addEventListener('DOMContentLoaded', function() {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
         
-            // const request = new XMLHttpRequest();
-            const formData = new FormData(form)
+            const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server.php', {
-                method: "POST",
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            })
-            .then(data => data.text)    
+            postData('http://localhost:3000/requests', json)
             .then(data => {
-                console.log(data)
-                showThanksModal()
-                statusMessage.remove()
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
             }).catch(() => {
                 showThanksModal(message.failure);
             }).finally(() => {
-                form.reset()
-            })
-
-            // request.open('POST', 's erver.php');
-            // request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-            // const formData = new FormData(form);
-            // request.send(json);
-
-            // request.addEventListener('load', () => {
-            //     if (request.status === 200) {
-            //         console.log(request.response);
-            //         showThanksModal(message.success);
-            //         statusMessage.remove();
-            //         form.reset();
-            //     } else {
-            //         showThanksModal(message.failure);
-            //     }
-            // });
+                form.reset();
+            });
         });
     }
 
@@ -304,44 +300,19 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-//Filter()
-const arr = ["Ivan", "Alex", "Andrey", "Arman"]
 
-const filt = arr.filter(function(name){
-    return name.length < 5 && name === "Ivan"
-})
+async function f() {
+    console.log('1')
 
-console.log(filt)
-
-//Map()
-const ans = ["IvAn", "AsYL", "Hello"]
-
-
-const fil = ans.map(item => item.toLowerCase())
-console.log(fil)
-
-//Some()
-const some = ["das", {asd}]
-
-console.log(some.some(item => typeof(item) === "number"))
-
-//Every()
-const com = [12,32,2,1231]
-console.log(com.every(item => item < 100))
-
-//Reduce()
-const arr1 = [3,32,3,3,2]
-                // sum = 0 , cur = 3 =>
-                // sum = 3, cur = 32 =>
-                // sum = 35, cur = 3 =>
-                // sum = 38, cur = 3 =>
-                // sum = 41, cur = 2 => 
-                // sum = 43, cur = none
-                // red = 43
-const red = arr1.reduce((sum,cur) => sum + cur)
-console.log(red)
-
-const fru = ["apple", "pear", "grape"]
-
-const out = fru.reduce((sum, cur) => `${sum} ${cur}`)
-console.log(out)
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve("готово!"), 1000)
+    });
+    
+    console.log(2)
+    let result = await promise; // будет ждать, пока промис не выполнится (*)
+  
+    console.log(3); // "готово!"
+    console.log(4)
+  }
+  
+f();
